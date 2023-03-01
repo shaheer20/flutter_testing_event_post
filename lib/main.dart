1a937ac5b92ea560aa6dd329fb1c337d6b47245d
macbookpro <shaheer20>
2023-03-01 13:05:37 +0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo for Cicd',
+      title: 'Flutter Demo for test',
       theme: ThemeData(
         // This is the theme of your application.
         //
